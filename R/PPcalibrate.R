fa5cae2cc79a80bcdc64408002003a921b328542
TJHeaton <t.heaton@leeds.ac.uk>
2023-11-14 13:07:34 +0000

    Fixing documentation

--- R/PPcalibrate.R
+++ R/PPcalibrate.R
@@ -1,15 +1,16 @@
 #' Title
 #'
 #' @param rc_determinations A vector of observed radiocarbon determinations
+#'
 #' @param rc_sigmas A vector of the radiocarbon determinations
 #' uncertainties (1-sigma). Must be the same length as `rc_determinations`.
+#'
 #' @param calibration_curve A dataframe which must contain one column `calendar_age_BP`, and also
 #' columns `c14_age` and `c14_sig` or `f14c` and `f14c_sig` (or both sets).
-#' This format matches the curves supplied with this package e.g. [carbondate::intcal20], [carbondate::intcal13],
-#' which contain all 5 columns.
+#' This format matches the curves supplied with this package
 #'
-#' @param use_F14C_space Whether the calculations are carried out in F14C space (default is TRUE).
-#' If FALSE, calculations are carried out in 14C yr BP space.
+#' @param F14C_inputs `TRUE` if the provided rc_determinations are F14C concentrations and `FALSE`
+#' if they are radiocarbon age BP. Defaults to `FALSE`.
 #'
 #' @param n_iter  The number of MCMC iterations (optional). Default is 100,000.
 #'
@@ -28,16 +29,19 @@
 #' adaptive prior on \eqn{\mu_{\phi}} and  (A, B).
 #' If this is `TRUE` (the default), then all the remaining arguments below are
 #' ignored.
-#' @param calendar_ages  The initial estimate for the underlying calendar ages
-#' (optional). If supplied it must be a vector with the same length as
-#' `rc_determinations`.  Required if `sensible_initialisation` is `FALSE`.
+#'
 #' @param s,h Parameters to define/specify the poisson process rate
 #' (jumps and heights)
 #'
-#' @return
+#' @param calendar_ages  The initial estimate for the underlying calendar ages
+#' (optional). If supplied it must be a vector with the same length as
+#' `rc_determinations`.  Required if `sensible_initialisation` is `FALSE`.
+#'
+#'
+#' @return TO DO
 #' @export
 #'
-#' @examples
+#' @examples TO DO
 PPcalibrate <- function(
     rc_determinations,
     rc_sigmas,
